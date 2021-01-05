ff0eeec89524a445094191c4e9dfc0dac1a68526
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2021-01-05 11:36:37 -0800

    add recursive preorder and iterative postorder traversal

--- tree.cs
+++ tree.cs
@@ -34,3 +34,46 @@ public List<int> Preorder(Node root) {
     }
     return result;      
 }
+
+public List<int> PreorderRecursive(Node root) {
+    var result = new List<int>();
+    Helper(root, result);
+    return result;
+}
+
+public void Helper(Node root, List<int> list)
+{
+    if(root == null)
+    {
+        return;
+    }
+    list.Add(root.val);
+    if(root.children != null)
+    {
+        foreach(var child in root.children)
+        {
+            Helper(child, list);
+        }
+    } 
+}
+
+public List<int> Postorder(Node root) {
+    var result = new List<int>();
+    if(root == null)
+    {
+        return result;
+    }
+    Stack<Node> stack = new Stack<Node>();
+    stack.Push(root);
+    while(stack.Count != 0)
+    {
+        var tempPop = stack.Pop();
+        result.Add(tempPop.val);
+        foreach(var child in tempPop.children)
+        {
+            stack.Push(child);
+        }           
+    }
+    result.Reverse();
+    return result;
+}
