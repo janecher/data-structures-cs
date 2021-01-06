ca69904abb688159842bfc2bec56fcb034c5fa3c
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2021-01-06 00:16:56 -0800

    add description for priority queue

--- heap-priority-queue.cs
+++ heap-priority-queue.cs
@@ -11,21 +11,28 @@ public class Node {
     }
 }
 
+public class PriorityNode {
+    public int val;
+    public int priority;
+    public left;
+    public TreeNode right;
+    public TreeNode(int val=0, int priority, TreeNode left=null, TreeNode right=null) {
+        this.priority = priority;
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 2n+1 - left child
 2n+2 - right child
 (n-1)/2 - parent
-
-public class MaxHeap {
-    public List<Node> values;
-    public MaxHeap() {
-        values = new List<Node>();
-    }
-}
 */
 
-public class MaxHeap {
+//in case of priority queue- dequeue and enqueue and comparing by priority!
+public class MaxBinaryHeap {
     public List<Node> values;
-    public MaxHeap() {
+    public MaxBinaryHeap() {
         values = new List<Node>();
     }
 
@@ -43,6 +50,40 @@ public class MaxHeap {
             Swap(values, parent, index);
             index = parent;
         }
+    }
+
+    public int ExtractMax()
+    {
+        int max = values[0];
+        Swap(values, 0, values.Count-1);
+        values.RemoveAt(rows.Count - 1);
+        if(values.Count == 0)
+        {
+            return max;
+        }
+        int index = 0;
+        while(index != -1 && index < values.Count)
+        {
+            int swap = -1;
+            int left = index * 2 + 1;
+            int right = index * 2 + 1;
+            if(left < values.Count && values[index] < values[left])
+            {
+                swap = left;
+            }
+            if(right < values.Count && ((swap == -1 && values[index] < values[right]) 
+                || (swap!= -1 && values[left] < values[right]))
+            {
+                swap = right;
+            }
+            if(swap == -1)
+            {
+                break;
+            }
+            Swap(values, swap, index);
+            index = swap;
+        }
+        return max;
     }
 
     public void Swap(List<Node> list, int i, int j)
